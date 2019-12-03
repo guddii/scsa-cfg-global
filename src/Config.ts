99a506bebbf2d692a9e4fd0279c91bb54d79cfb3
Florian Gudat <florian.gudat@guices.com>
2019-12-03 20:02:46 +0100

    Add config branch

--- src/Config.ts
+++ src/Config.ts
@@ -1,13 +1,6 @@
 import * as settings from "./settings";
 import { Application } from "./components";
 
-const convert = (map: any) =>
-  Array.from(map).reduce((obj, [key, value]) => {
-    // @ts-ignore
-    obj[key] = value;
-    return obj;
-  }, {});
-
 export class Config {
   public MODE: string;
   public CURRENT: Application;
@@ -16,12 +9,18 @@ export class Config {
   public NODE_ENV: string;
   public PORT: string;
 
-  constructor(current: string) {
+  /**
+   * Instantiate configuration
+   *
+   * @param current Selects a configuration from the list of configurations.
+   * @param from Use a different object to pick from than the orchestrator object.
+   */
+  constructor(current: string, from: string = "orchestrators") {
     this.MODE = process.env.MODE || "dev";
     const ConfigByMode: any = settings[this.MODE].options;
     this.ORCHESTRATORS = ConfigByMode.orchestrators;
     this.APPLICATIONS = ConfigByMode.applications;
-    this.CURRENT = ConfigByMode.orchestrators[current];
+    this.CURRENT = ConfigByMode[from][current];
     this.NODE_ENV = process.env.NODE_ENV || "development";
     this.PORT = process.env.PORT || this.CURRENT.options.url.port;
   }
