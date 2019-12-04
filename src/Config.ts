0605aeec222d5a4e4216d94bc86b24c801a4b9aa
Florian Gudat <florian.gudat@guices.com>
2019-12-04 11:19:30 +0100

    Add parent to config

--- src/Config.ts
+++ src/Config.ts
@@ -1,9 +1,10 @@
 import * as settings from "./settings";
-import { Application } from "./components";
+import { Application, Orchestrator } from "./components";
 
 export class Config {
   public MODE: string;
   public CURRENT: Application;
+  public PARENT: Orchestrator;
   public ORCHESTRATORS: any; // Map<String, Orchestrator>;
   public APPLICATIONS: any; // Application;
   public NODE_ENV: string;
@@ -14,13 +15,15 @@ export class Config {
    *
    * @param current Selects a configuration from the list of configurations.
    * @param from Use a different object to pick from than the orchestrator object.
+   * @param parent
    */
-  constructor(current: string, from: string = "orchestrators") {
+  constructor(current: string, from: string = "orchestrators", parent: string = "WebComponents") {
     this.MODE = process.env.MODE || "dev";
     const ConfigByMode: any = settings[this.MODE].options;
     this.ORCHESTRATORS = ConfigByMode.orchestrators;
     this.APPLICATIONS = ConfigByMode.applications;
     this.CURRENT = ConfigByMode[from][current];
+    this.PARENT = ConfigByMode["orchestrators"][parent];
     this.NODE_ENV = process.env.NODE_ENV || "development";
     this.PORT = process.env.PORT || this.CURRENT.options.url.port;
   }
